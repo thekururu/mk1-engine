812ae5aacc192caed073ef39255e31cd3c5b0cd1
MyThekuruku <thekuruky@gmail.com>
2026-01-21 14:48:58 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+﻿const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let loopId;
+let update = () => {};
+let draw = () => {};
+
+const files = {
+  scripts: {
+    "main.js": `
+let sun = { x: 300, y: 120, radius: 40 };
+let moon = { x: 0, y: 120, radius: 30, speed: 0.5 };
+
+function update() {
+  moon.x += moon.speed;
+  if (moon.x > 700) moon.x = -100;
+}
+
+function draw(ctx) {
+  ctx.fillStyle = "#5bbcff";
+  ctx.fillRect(0,0,600,240);
+
+  ctx.fillStyle = "yellow";
+  ctx.beginPath();
+  ctx.arc(sun.x, sun.y, sun.radius, 0, Math.PI*2);
+  ctx.fill();
+
+  ctx.fillStyle = "#555";
+  ctx.beginPath();
+  ctx.arc(moon.x, moon.y, moon.radius, 0, Math.PI*2);
+  ctx.fill();
+}
+`
+  }
+};
+
+function renderFiles() {
+  const container = document.getElementById("files");
+  container.innerHTML = "📁 scripts<br>";
+  container.innerHTML += `
+    <div class="file" onclick="openFile('scripts','main.js')">
+      📄 main.js
+    </div>
+  `;
+}
+
+function openFile(folder, file) {
+  document.getElementById("code").value = files[folder][file];
+}
+
+function run() {
+  cancelAnimationFrame(loopId);
+  try {
+    eval(code.value);
+  } catch (e) {
+    alert(e);
+    return;
+  }
+
+  function loop() {
+    ctx.clearRect(0,0,600,240);
+    update();
+    draw(ctx);
+    loopId = requestAnimationFrame(loop);
+  }
+  loop();
+}
+
+function stop() {
+  cancelAnimationFrame(loopId);
+}
+
+renderFiles();
+openFile("scripts","main.js");
